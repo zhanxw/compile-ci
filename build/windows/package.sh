5d597be5e6a1e579b894652c921146996894e085
zhanxw <zhanxw@gmail.com>
2018-10-09 12:56:45 -0500

    fix windows

--- build/windows/package.sh
+++ build/windows/package.sh
@@ -2,24 +2,26 @@
 
 set -xue
 
-strip build-dir/battleship.exe
-upx -9 build-dir/battleship.exe
+# strip build-dir/battleship.exe
+# upx -9 build-dir/battleship.exe
 
-PORTABLE_FILE=battleship_@VERSION@_${BATTLESHIP_PLATFORM}-portable.exe
+DEPLOY_FILE=dist/test_${PLATFORM}.zip
+mv executable/main executable/main.exe
+zip $DEPLOY_FILE executable/*.exe
 
-cp build-dir/battleship.exe $PORTABLE_FILE
+# cp build-dir/battleship.exe $PORTABLE_FILE
 
-cp @PROJECT_SOURCE_DIR@/src/view/images/battleship.ico build-dir
+# cp @PROJECT_SOURCE_DIR@/src/view/images/battleship.ico build-dir
 
-awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/README.md > \
-    build-dir/README.txt
-awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/LICENSE > \
-    build-dir/LICENSE.txt
-awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/AUTHORS > \
-    build-dir/AUTHORS.txt
+# awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/README.md > \
+#     build-dir/README.txt
+# awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/LICENSE > \
+#     build-dir/LICENSE.txt
+# awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/AUTHORS > \
+#     build-dir/AUTHORS.txt
 
-if [ -f "build-dir/wizard.nsi" ]; then
-    build-dir/nsis_wizard.sh
-fi
+# if [ -f "build-dir/wizard.nsi" ]; then
+#     build-dir/nsis_wizard.sh
+# fi
 
 set +xue
